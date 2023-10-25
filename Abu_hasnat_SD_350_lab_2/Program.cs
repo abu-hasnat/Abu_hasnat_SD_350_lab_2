5a3d055562981efba6607e876a1f34525c734ada
Abu Hasnat <hasnat.tushar@gmail.com>
2023-10-25 07:40:34 -0500

    Add project files.

---
+++ Abu_hasnat_SD_350_lab_2/Program.cs
@@ -0,0 +1,26 @@
+﻿public abstract class Client
+{
+    public string UserName {  get; set; }
+    public string Email { get; set; }
+    public string Description { get; set; } = "No Description";
+    public virtual string GetDescription()
+    {
+        return Description;
+    }
+}
+
+public class User: Client
+{
+    public User(string username, string email)
+    {
+        username = username;
+        Email = email;
+        Description = "Base-level User";
+    }
+}
+
+public abstract class BadgeDecorator: Client
+{
+    public Client Cleint { get; set }
+    public abstract override string GetBadges(); //
+}
